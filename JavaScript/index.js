3e95595a91533f585f9a42788fc5c6d836be646f
PeterNgKP <1831699658qaz123@gmail.com>
2023-01-29 23:03:23 +0800

    Update

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,15 +1,20 @@
 // Loading function
+function RenderElement(className, HTMLText_Url){
+    $.get(HTMLText_Url, function (html_text) {
+        $(className).html(html_text);
+   });
+
+}
+
 $(window).on("load", function() { 
     $("#loadingImg h5").text("Ready").delay(500).fadeOut(500); 
     $("#loadingImg img").delay(500).fadeOut(500); 
-    $("#preloader").delay(800).fadeOut(1000); 
+    $("#preloader").delay(800).fadeOut(700); 
     setInterval(function(){
         $("#preloader").remove(); 
+        RenderElement("#header", "./Snippet/Header.html");
+        RenderElement("#main", "./Snippet/Main.html");
+        RenderElement("#footer", "./Snippet/Footer.html");
     }, 2500);
 });
 
-$.get("./Snippet/Header.html", function (result) {
-     html = result;
-     
-     print(html)
-});
