f5f579a7a2c77677018568e6de93e065c94911f5
PeterNgKP <1831699658qaz123@gmail.com>
2023-01-30 01:08:17 +0800

    One section and One page left

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -3,18 +3,19 @@ function RenderElement(className, HTMLText_Url){
     $.get(HTMLText_Url, function (html_text) {
         $(className).html(html_text);
    });
-
 }
 
 $(window).on("load", function() { 
     $("#loadingImg h5").text("Ready").delay(500).fadeOut(500); 
     $("#loadingImg img").delay(500).fadeOut(500); 
     $("#preloader").delay(800).fadeOut(700); 
-    setInterval(function(){
+    setTimeout(function(){
         $("#preloader").remove(); 
         RenderElement("#header", "./Snippet/Header.html");
         RenderElement("#main", "./Snippet/Main.html");
         RenderElement("#footer", "./Snippet/Footer.html");
     }, 1200);
 });
+
+
 
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# IERG-4210Web-Programming-and-Security
-Assignment_For_IERG 4210 Web Programming and Security
+
