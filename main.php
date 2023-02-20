16acad38169153dd5a1b44e7cadda12bb3cc0f44
PeterNgKP <95013548+PeterNg2333@users.noreply.github.com>
2023-02-20 21:41:53 +0800

    update HTML

--- main.php
+++ main.php
@@ -5,7 +5,9 @@ $res = ierg4210_cat_fetchall();
 $products = '<ul>';
 
 foreach ($res as $value){
-    $products .= '<li><a href = "'.$value["CID"].'"> '.$value["CATEGORIES_NAME"].'</a></li>';
+    // $products .= '<li><a href = "'.$value["CID"].'"> '.$value["CATEGORIES_NAME"].'</a></li>';
+    //
+    $products .= '<li><a href = "./#Food_Category"> '.$value["CATEGORIES_NAME"].'</a></li>';
 }
 
 $products .= '</ul>';
