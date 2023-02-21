4f4860158b6acd8ca30c0ca792422d7da5629399
PeterNgKP <95013548+PeterNg2333@users.noreply.github.com>
2023-02-21 08:21:44 +0800

    Update admin.php

--- admin/admin.php
+++ admin/admin.php
@@ -18,6 +18,8 @@ foreach ($res as $value){
             <label for="prod_name"> Name *</label>
             <div> <input id="prod_name" type="text" name="name" required="required" pattern="^[\w\-]+$"/></div>
             <label for="prod_price"> Price *</label>
+            <div> <input id="prod_inv" type="text" name="price" required="required" pattern="^[\d+]+$"/></div>
+            <label for="prod_inv"> Inventory </label>
             <div> <input id="prod_price" type="text" name="price" required="required" pattern="^\d+\.?\d*$"/></div>
             <label for="prod_desc"> Description *</label>
             <div> <input id="prod_desc" type="text" name="description"/> </div>
