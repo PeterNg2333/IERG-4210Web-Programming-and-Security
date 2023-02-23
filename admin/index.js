0e08245bf4913c01e83af9cb5d01b8dead2ccf45
PeterNgKP <95013548+PeterNg2333@users.noreply.github.com>
2023-02-23 13:45:05 +0800

    Update index.js

--- admin/index.js
+++ admin/index.js
@@ -89,7 +89,8 @@ function edit_category(){
 function change_category(){
     var selectement = document.getElementById('Category_dropDown');
     document.getElementById('cid_input').value = selectement.value;
-    document.getElementById('cid_display_name').value = selectement.name;
+    // document.getElementById('cid_display_name').value = selectement.name;
+    document.getElementById('cid_display_name').value = selectement.text;
     // alert(document.getElementById('cid_input').value);
     load_products_by_cid();
 }
