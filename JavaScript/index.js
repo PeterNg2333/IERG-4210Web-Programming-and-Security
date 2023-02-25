7a099108c111f738520f12ee51c9e6874fb33e29
PeterNgKP <95013548+PeterNg2333@users.noreply.github.com>
2023-02-26 01:34:04 +0800

    a

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -138,28 +138,29 @@ window.addEventListener('scroll', ()=>{
                             var get_price = element.PRICE;
                             var get_pName = element.PRODUCT_NAME;
 
-                            $product += '<div class="count_product_loaded" col-lg-3 mb-3 px-0" id="P-'+get_pid +'">';
-                            $product += '    <div class="card mx-2 product_card_display">';
-                            $product += '        <a href="/main.php?pid='+ get_pid +'" class="product_detail_button">';
-                            $product += '             <img class="card-img-top" src="./admin/lib/images/P'+get_pid+'.jpg" alt="'+get_pName+'" id="imageP-'+get_pid+'">';
-                            $product += '        </a>';
-                            $product += '        <div class="card-body card_display_body row">';
-                            $product += '           <div class="row">';
-                            $product += '               <h5 class="product_detail_button card-title col-8"><a href="/main.php?pid='+get_pid+'" id="titleP-'+get_pid+'">'+get_pName+'</a></h5>';
-                            $product += '               <p class="card-text col-4">' + get_price + '</p>';
-                            $product += '           </div>';
-                            $product += '           <input id="addToCartNum-'+get_pid+'" type=hidden value="1" />';
-                            $product += '           <button type="button" id="addToCart-'+get_pid+'" class="addToCart btn btn-primary btn-block product_card_display_button"> Add to Shopping Cart</button>';
-                            $product += '        </div>';
-                            $product += '   </div>';
-                            $product += '</div>';
+                            product_html += '<div class="count_product_loaded" col-lg-3 mb-3 px-0" id="P-'+get_pid +'">';
+                            product_html += '    <div class="card mx-2 product_card_display">';
+                            product_html += '        <a href="/main.php?pid='+ get_pid +'" class="product_detail_button">';
+                            product_html += '             <img class="card-img-top" src="./admin/lib/images/P'+get_pid+'.jpg" alt="'+get_pName+'" id="imageP-'+get_pid+'">';
+                            product_html += '        </a>';
+                            product_html += '        <div class="card-body card_display_body row">';
+                            product_html += '           <div class="row">';
+                            product_html += '               <h5 class="product_detail_button card-title col-8"><a href="/main.php?pid='+get_pid+'" id="titleP-'+get_pid+'">'+get_pName+'</a></h5>';
+                            product_html += '               <p class="card-text col-4">' + get_price + '</p>';
+                            product_html += '           </div>';
+                            product_html += '           <input id="addToCartNum-'+get_pid+'" type=hidden value="1" />';
+                            product_html += '           <button type="button" id="addToCart-'+get_pid+'" class="addToCart btn btn-primary btn-block product_card_display_button"> Add to Shopping Cart</button>';
+                            product_html += '        </div>';
+                            product_html += '   </div>';
+                            product_html += '</div>';
 
                         }
                         else{
                             console.log("Unloaded: " + load_count);
                         }
-                        load_count ++;
                         
+                        load_count ++;
+                        $("#product").after(product_html);
                     });
                 }
 
