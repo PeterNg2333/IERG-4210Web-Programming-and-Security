4f1e5ca451806a90155538f7864dfda5cb4c2049
PeterNgKP <95013548+PeterNg2333@users.noreply.github.com>
2023-02-25 14:37:40 +0800

    Update index.js

--- admin/index.js
+++ admin/index.js
@@ -323,7 +323,7 @@ function enable_modify(e){
 
 function file_check(img_name){
     
-    var file_name = img_name.split(".");
+    var file_name = img_name.split(",");
     print("image uploead:" + file_name);
     file_type = file_name[file_name.length-1];
     print("file type:" + file_name);
