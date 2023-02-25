7ede90caa9e9f8fc502d2a1c4a355145b65e99cb
PeterNgKP <95013548+PeterNg2333@users.noreply.github.com>
2023-02-25 09:32:14 +0800

    Update db.inc.php

--- admin/lib/db.inc.php
+++ admin/lib/db.inc.php
@@ -405,9 +405,16 @@ function ierg4210_prod_fetch_four_page(){
     // DB manipulation
     global $db;
     $db = ierg4210_DB();
-    $q = $db->prepare("SELECT * FROM PRODUCTS LEFT JOIN (SELECT PID, row_number() OVER() AS row_num FROM PRODUCTS) USING(PID) WHERE ROW_NUM <= 4;");
+    $q = $db->prepare("SELECT * FROM PRODUCTS LEFT JOIN (SELECT PID, row_number() OVER() as row_num FROM PRODUCTS) USING(PID);");
     if ($q->execute())
-        return $q->fetchAll();
+         return $q->fetchAll();
+
+    // // DB manipulation
+    // global $db;
+    // $db = ierg4210_DB();
+    // $q = $db->prepare("SELECT * FROM PRODUCTS LEFT JOIN (SELECT PID, row_number() OVER() as row_num FROM PRODUCTS) USING(PID) Where ROW_NUM <= 4;");
+    // if ($q->execute())
+    //     return $q->fetchAll();
 }
 
 function ierg4210_prod_count_limit(){
