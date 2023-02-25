92a65f2322ff7fe8c4b5ca4beea63593fb1cb26c
PeterNgKP <95013548+PeterNg2333@users.noreply.github.com>
2023-02-25 09:37:01 +0800

    Update db.inc.php

--- admin/lib/db.inc.php
+++ admin/lib/db.inc.php
@@ -405,7 +405,7 @@ function ierg4210_prod_fetch_four_page(){
     // DB manipulation
     global $db;
     $db = ierg4210_DB();
-    $q = $db->prepare("SELECT * FROM PRODUCTS;");
+    $q = $db->prepare("SELECT * FROM PRODUCTS LEFT JOIN (SELECT PID, (row_number() OVER()) as row_num FROM PRODUCTS) USING(PID) Where ROW_NUM <= 4;");
     if ($q->execute())
          return $q->fetchAll();
 
