6a3d20576f0c979b1b267be44903772815de3624
PeterNgKP <95013548+PeterNg2333@users.noreply.github.com>
2023-02-27 13:08:32 +0800

    debug-0

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -51,7 +51,7 @@ function addToCart_button(e){
     var message = ""
     var text_array = e.target.id.split("-");
     var inputed_id = text_array[1];
-    var num_added = document.querySelector("#addToCartNum-" + id).value;
+    var num_added = document.querySelector("#addToCartNum-" + inputed_id).value;
     if (num_added != 0){
         // load local Storage
         var jsonstr = localStorage.getItem('shoppingList');
