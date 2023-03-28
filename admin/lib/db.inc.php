e155a652e15f10746f325d02692863cad5e2d6b6
PeterNgKP <95013548+PeterNg2333@users.noreply.github.com>
2023-03-29 07:05:30 +0800

    Update db.inc.php

--- admin/lib/db.inc.php
+++ admin/lib/db.inc.php
@@ -222,7 +222,7 @@ function ierg4210_prod_edit(){
         $q->bindParam(5, $desc);
         $q->bindParam(6, $pid);
         $filePath = "/var/www/IERG-4210Web-Programming-and-Security/admin/lib/images/P" . $pid . ".jpg";
-        echo $_FILES["file"]["type"];
+        echo empty($_FILES["file"]);
         
         // Copy the uploaded file to a folder which can be publicly accessible at incl/img/[pid].jpg
         if ($_FILES["file"]["error"] == 0
