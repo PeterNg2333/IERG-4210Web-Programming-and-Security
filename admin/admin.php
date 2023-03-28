390e47ec7a9086f8076ef59e81e4ab17bf020965
PeterNgKP <95013548+PeterNg2333@users.noreply.github.com>
2023-03-28 21:06:52 +0800

    Update admin.php

--- admin/admin.php
+++ admin/admin.php
@@ -41,7 +41,7 @@ foreach ($res_cat_fetchall as $value){
             <?php 
                 $header_html = file_get_contents('./Snippet_admin/header_admin.html');
                 echo $header_html;
-                echo $res_number['CATEGORY_NUM'];
+                echo $res_number;
             ?>
         </header>
 
