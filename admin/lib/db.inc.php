45c56cbdf4ed375c6d2dfe24ae6eeeb2560a4149
PeterNgKP <95013548+PeterNg2333@users.noreply.github.com>
2023-03-29 13:09:45 +0800

    Update db.inc.php

--- admin/lib/db.inc.php
+++ admin/lib/db.inc.php
@@ -597,18 +597,18 @@ function ierg4210_changePd(){
 }
 
 function ierg4210_exit(){
-    // unset($_COOKIE['auth']);
-    // $exp = time() - 3600 * 24 * 1;
-    // setcookie('auth', json_encode(""), $exp, "/", "secure.s19.ierg4210.ie.cuhk.edu.hk", true, true);
+    unset($_COOKIE['auth']);
+    $exp = time() - 3600 * 24 * 1;
+    setcookie('auth', json_encode(""), $exp, "/", "secure.s19.ierg4210.ie.cuhk.edu.hk", true, true);
 
-    // unset($_SESSION['auth']);
-    // header('Location: login_admin.html', true, 302);
-    // exit();
+    unset($_SESSION['auth']);
+    header('Location: login_admin.html', true, 302);
+    exit();
 
 
     // temp
-    unset($_SESSION['auth']);
-    return auth();
+    // unset($_SESSION['auth']);
+    // return auth();
 }
 
 
