f43c0155604806f8711df7c05acf4ba37b4f899a
PeterNgKP <95013548+PeterNg2333@users.noreply.github.com>
2023-03-29 13:58:30 +0800

    Update db.inc.php

--- admin/lib/db.inc.php
+++ admin/lib/db.inc.php
@@ -641,7 +641,7 @@ function auth(){
             $user_salt = $user["SALT"]; 
             $user_flag = $user["FLAG"]; 
              
-            if ($k == hash_hmac('sha256', $exp . $user_password, $user_salt)){
+            if ($k == hash_hmac('sha256', $exp . $user_password, $user_salt && $user_flag == 1)){
                 $_SESSION['auth'] = $_COOKIE['auth'];
                 return $user_email;
             } else {
