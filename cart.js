3abc0278cbe486aec94a572b977d2ec9c7fc50aa
PeterNgKP <95013548+PeterNg2333@users.noreply.github.com>
2023-04-02 17:01:02 +0800

    Payment

---
+++ cart.js
@@ -0,0 +1,36 @@
+/**
+ * This function returns all items in the cart. 
+ * @return an array containing items (pid + quantity)
+ */
+function getCartItems() {
+  /* @TODO Comment out the current return statement */
+  /* ========== REGION START ========== */
+  return [
+    {
+      pid: 1, quantity: 1,
+    },
+    {
+      pid: 2, quantity: 2
+    }
+  ];
+  /* ========== REGION END ========== */
+
+  /* @TODO Your Implementation Here. */
+  /* ========== REGION START ========== */
+
+
+
+  /* ========== REGION END ========== */
+}
+
+/**
+ * This function clears all items in the cart. 
+ */
+function clearCart() {
+  /* @TODO Your Implementation Here. */
+  /* ========== REGION START ========== */
+
+
+
+  /* ========== REGION END ========== */ 
+}
