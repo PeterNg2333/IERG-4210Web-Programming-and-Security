a0e8f13ffd0d73c416d9db94a83157cd97e07970
PeterNgKP <95013548+PeterNg2333@users.noreply.github.com>
2023-04-07 18:31:51 +0800

    Update create_order.php

--- create_order.php
+++ create_order.php
@@ -37,55 +37,63 @@ function create_order($cart)
 {
   /* @TODO Comment out the current return statement */
   /* ========== REGION START ========== */
-  $json = '{
-    "purchase_units": [
-      {
-        "amount": {
-          "currency_code": "USD",
-          "value": 11,
-          "breakdown": {
-            "item_total": {
-              "currency_code": "USD",
-              "value": 11
-            }
-          }
-        },
-        "items": [
-          {
-            "name": "1:ProductA",
-            "unit_amount": {
-              "currency_code": "USD",
-              "value": 1
-            },
-            "quantity": 1
-          },
-          {
-            "name": "2:ProductB",
-            "unit_amount": {
-              "currency_code": "USD",
-              "value": 2
-            },
-            "quantity": 5
-          }
-        ]
-      }
-    ]
-  }';
+  // $json = '{
+  //   "purchase_units": [
+  //     {
+  //       "amount": {
+  //         "currency_code": "USD",
+  //         "value": 11,
+  //         "breakdown": {
+  //           "item_total": {
+  //             "currency_code": "USD",
+  //             "value": 11
+  //           }
+  //         }
+  //       },
+  //       "items": [
+  //         {
+  //           "name": "1:ProductA",
+  //           "unit_amount": {
+  //             "currency_code": "USD",
+  //             "value": 1
+  //           },
+  //           "quantity": 1
+  //         },
+  //         {
+  //           "name": "2:ProductB",
+  //           "unit_amount": {
+  //             "currency_code": "USD",
+  //             "value": 2
+  //           },
+  //           "quantity": 5
+  //         }
+  //       ]
+  //     }
+  //   ]
+  // }';
   
 
-  $order = json_decode($json);
+  // $order = json_decode($json);
 
-  $order->purchase_units[0]->custom_id = gen_digest(array($order->purchase_units[0]->amount->currency_code));
-  $order->purchase_units[0]->invoice_id = gen_uuid(); // invoice_id must be unique to avoid crashes.
+  // $order->purchase_units[0]->custom_id = gen_digest(array($order->purchase_units[0]->amount->currency_code));
+  // $order->purchase_units[0]->invoice_id = gen_uuid(); // invoice_id must be unique to avoid crashes.
 
-  return json_encode($order);
+  // return json_encode($order);
   /* ========== REGION END ========== */
 
   /* @TODO Your Implementation here */
   /* ========== REGION START ========== */
+  $json = [];
+  echo $cart;
+  exit;
 
 
+  // $order = json_decode($json);
 
+  // $order->purchase_units[0]->custom_id = gen_digest(array($order->purchase_units[0]->amount->currency_code));
+  // $order->purchase_units[0]->invoice_id = gen_uuid(); // invoice_id must be unique to avoid crashes.
+
+  // return json_encode($order);
   /* ========== REGION END ========== */
 }
 
