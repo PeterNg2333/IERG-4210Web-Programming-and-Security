410b3031e9209f28c92dc488776ed42676489c3d
PeterNgKP <95013548+PeterNg2333@users.noreply.github.com>
2023-04-08 10:33:08 +0800

    Update user_order.js

--- JavaScript/user_order.js
+++ JavaScript/user_order.js
@@ -1,2 +1,2 @@
-document.querySelector("#user_order > a").href = "main.php";
-document.querySelector("#user_order > a").innerHTML = '<i class="far fa-home"></i><span>Home </span>'
+// document.querySelector("#user_order>a").href = "main.php";
+// document.querySelector("#user_order>a").innerHTML = '<i class="far fa-home"></i><span>Home </span>'
