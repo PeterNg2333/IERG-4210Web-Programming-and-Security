40123f299d64c29c158c6871611dad84a36e9ec4
PeterNgKP <95013548+PeterNg2333@users.noreply.github.com>
2023-04-08 11:55:05 +0800

    Update order_admin.php

--- admin/order_admin.php
+++ admin/order_admin.php
@@ -81,5 +81,6 @@ if (is_admin($auth) == false){
 
     <script src="https://code.jquery.com/jquery-3.6.3.js" integrity="sha256-nQLuAZGRRcILA+6dMBOvcRh5Pe310sBpanc6+QBmyVM=" crossorigin="anonymous"></script>
     <script src="../JavaScript/utity.js"></script>
+    <script src="./index.js"></script>
     </body>
 </html>
