10beaba7a7f0e260f7469a13f22d7dda96988b97
PeterNgKP <95013548+PeterNg2333@users.noreply.github.com>
2023-04-13 12:14:34 +0800

    Update index.js

--- admin/index.js
+++ admin/index.js
@@ -62,6 +62,9 @@ function add_category(){
          nonce: _nonce
         },
         function(json){
+            var jsonStr = json.replace(/^while\(1\);/, "");
+            var jsonObj = JSON.parse(jsonStr);
+            print (jsonObj);
             if (json.status == "Success"){
                 alert("Add Success!!");
                 location.reload()
